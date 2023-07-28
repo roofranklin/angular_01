dbdd928b1f5183bf9d4c22a2f4ea9deae9d97f75
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2023-07-28 16:37:31 -0300

    Criando componente cabeçalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+// MEUS BLOCOS DE CÓDIGO
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 import { ContatoComponent } from './contato/contato.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// MATERIAL UI
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuemSomosComponent,
-    ContatoComponent
+    ContatoComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,

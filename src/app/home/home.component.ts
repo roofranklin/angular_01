fc310fabd6edcf615a882b9df935ff29cca4a646
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2023-08-16 17:00:33 -0300

    Função de favoritos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,23 +8,34 @@ import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarModule,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [MatGridListModule, MatCardModule, MatIconModule, NgFor, NgIf, CommonModule ],
+  imports: [MatGridListModule, MatCardModule, MatIconModule, NgFor, NgIf, CommonModule,  MatSnackBarModule ],
 })
 export class HomeComponent implements OnInit {
 
   nome: string = 'Pedro';
   sobrenome: string = 'dos Santos';
   imoveis: any;
+  imovel: any;
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
   constructor(
     private router: Router,
-    private http: HttpClient
-   
+    private http: HttpClient,
+    private _snackBar: MatSnackBar 
   ) { }
 
   ngOnInit(): void {
@@ -37,7 +48,39 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/detalhes', imovelId]);
   }
 
-  toogleFavorito(index: number): void {
-    this.imoveis[index].favorito = !this.imoveis[index].favorito
-  }
+  toogleFavorito(imovelId: string): void {
+
+    this.http.get<any>('http://localhost:3000/imoveis/' + imovelId).subscribe(data => {
+
+      this.imovel = data;  
+      this.imovel.favorito = !this.imovel.favorito;
+
+      this.http.patch('http://localhost:3000/imoveis/' + imovelId, { favorito: this.imovel.favorito })
+        .subscribe(
+          response => {
+            if (this.imovel.favorito === true) {
+              this._snackBar.open('O imóvel foi favoritado!', 'Fechar', {
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition,
+                duration: 5000
+              });
+            } else {
+              this._snackBar.open('O imóvel foi removido dos favoritos...', 'Fechar', {
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition,
+                duration: 5000
+              });
+            }
+            this.http.get<any>('http://localhost:3000/imoveis').subscribe(data => {
+              this.imoveis = data;
+            });
+          },
+          error => {
+              console.log('Ocorreu um erro', error);
+              this.imovel.favorito = !this.imovel.favorito;
+          } 
+            
+        );
+      });
+    }
 }
